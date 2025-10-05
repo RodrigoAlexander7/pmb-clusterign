8b828ce728fa6a098bc18f1d4b1bfe0ffcaefe02
RodryAlexander <rodrygoleu7@gmail.com>
2025-10-05 12:26:10 -0500

    Adding route to keywords by id

--- app/routers/data.py
+++ app/routers/data.py
@@ -1,9 +1,25 @@
 from fastapi import APIRouter
+from app.utils.base_dir import BASE_DIR
+import json
 from app.services.data import get_title_by_id
 
+
 router = APIRouter(prefix="/data", tags=["data"])
+keywords_per_doc_path = BASE_DIR/'data'/'processed'/'keywordsPerDoc.json' 
 
 
 @router.get('/titleById')
 async def getTitleById(id:str):
-    get_title_by_id(id)
+    return get_title_by_id(id)
+
+@router.get('/keywordsById')
+async def getKeywordsById(id:str):
+    title = get_title_by_id(id)
+    keywords_per_doc_dict = {}
+    with open(keywords_per_doc_path, 'r') as d:
+        keywords_per_doc_dict = json.load(d)
+    return keywords_per_doc_dict[title]
+    
+
+
+
