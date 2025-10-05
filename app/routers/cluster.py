ac3e78958d60484dc8d5b1e9f40889d7da247489
RodryAlexander <rodrygoleu7@gmail.com>
2025-10-05 09:18:22 -0500

    Route to unigram keywords

--- app/routers/cluster.py
+++ app/routers/cluster.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
 from app.services.clustering import get_cluster_json
+from app.services.clustering import get_bigrams_clusters_labeled_json
 
 router = APIRouter(prefix="/cluster", tags=["cluster"])
 
-@router.get('/')
+@router.get('/docs')
 async def getClusters():
     return get_cluster_json()
+
+@router.get('/bigramKeywords')
+async def getKeywordsBigram():
+    return get_bigrams_clusters_labeled_json()
--- app/services/clustering.py
+++ app/services/clustering.py
@@ -9,6 +9,22 @@ def get_cluster_json():
         data = json.load(f)
     return data
 
+b_kwr_path = BASE_DIR/'data'/'processed'/'bigramKeywords.json' 
+u_kwr_path = BASE_DIR/'data'/'processed'/'unigramKeywordsKeyBERT.json' 
+
+def get_bigrams_clusters_labeled_json():
+    num_kwr = {}    #dict with the number of cluster and the keywords list asociate
+    with open(u_kwr_path, 'r') as f:
+        num_kwr = json.load(f)
+    return num_kwr
+    
+    
+
+    
+    
+    
+
+
 
 
 
